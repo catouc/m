f1495828600a7dd9a3bf3594142495da8b7738d4
Philipp Böschen <catouc@philipp.boeschen.me>
2022-07-17 18:21:12 +0200

    Add module vendoring because that seems like a good idea

--- go.mod
+++ go.mod
@@ -31,5 +31,6 @@ require (
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20220602131408-e326c6e8e9c8 // indirect
 	google.golang.org/grpc v1.47.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ vendor/github.com/ardanlabs/conf/v2/yaml/yaml.go
@@ -0,0 +1,44 @@
+// Package yaml provides yaml support for conf.
+package yaml
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+
+	"gopkg.in/yaml.v2"
+)
+
+// YAML provides support for unmarshaling YAML into the applications
+// config value. After the yaml is unmarshaled, the Parse function is
+// executed to apply defaults and overrides. Fields that are not set to
+// their zero after the yaml is parsed will have the defaults ignored.
+type YAML struct {
+	data []byte
+}
+
+// WithData accepts the yaml document as a slice of bytes.
+func WithData(data []byte) YAML {
+	return YAML{
+		data: data,
+	}
+}
+
+// WithReader accepts a reader to read the yaml.
+func WithReader(r io.Reader) YAML {
+	var b bytes.Buffer
+	if _, err := b.ReadFrom(r); err != nil {
+		return YAML{}
+	}
+
+	return WithData(b.Bytes())
+}
+
+// Process performs the actual processing of the yaml.
+func (y YAML) Process(prefix string, cfg interface{}) error {
+	err := yaml.Unmarshal(y.data, cfg)
+	if err != nil {
+		return fmt.Errorf("unmarshal yaml: %w", err)
+	}
+	return nil
+}
